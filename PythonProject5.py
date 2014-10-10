d77651130a03c2eddcc04d3595add49b35e46262
rnyorke <rnyorke@gmail.com>
2014-10-10 14:43:35 -0500

    Lab5

---
+++ PythonProject5.py
@@ -0,0 +1,16 @@
+myList = [1,2,3,4,5,6,7,8,9,10]
+for a in myList:
+    print a
+
+for i in range(11,21,1):
+    print i
+    
+for b in range(99,0,-2):
+    print b
+aList = []    
+for e in range(0,5,1):
+    print "Enter a number" 
+    userinput = raw_input()
+    aList.append(userinput)
+for c in aList:
+    print c
